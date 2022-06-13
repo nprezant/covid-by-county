bfc03699bde43e9a5fd3637db3f8756c0e71a948
boom-roasted <noah.prezant@protonmail.ch>
2022-06-12 23:19:08 -0600

    Add crosswalk generation

--- README.md
+++ README.md
@@ -29,3 +29,36 @@ Can use for *suggestions* when typing a county/state
 Update times...
 https://api.us.socrata.com/api/catalog/v1
 https://opendata.stackexchange.com/questions/10761/get-metadata-from-all-datasets-on-socrata
+
+# Searching for Counties
+
+Need to be able to search state, county, city, or zip.
+
+Can get crosswalk for state --> county:
+https://www.nber.org/research/data/ssa-federal-information-processing-series-fips-state-and-county-crosswalk
+Name: crosswalks/raw/county-state-crosswalk.csv
+
+Can get crosswalk for zip code --> county --> state:
+https://www.kaggle.com/datasets/danofer/zipcodes-county-fips-crosswalk
+Name: crosswalks/raw/zip-county.csv
+
+Can get crosswalk for place (city) --> county:
+https://data.world/nrippner/ansi-geographic-codes/workspace/file?filename=ansi_places.csv
+Name: crosswalks/raw/ansi-places.csv
+
+Want to be able to look up anything and get a county in return.
+
+- Middlesex County, Colorado 80518 80519 Lansing
+- <county>, <long-state> <zips...> <places...>
+
+May be able to create an index for faster searches.
+For each alphabetic character and each number, score each county
+
+Questions:
+
+- How many unique counties are there?
+- Zips associated with each county
+- Places associated with each county
+
+Characters to clean:
+�
---
+++ crosswalks/process.py
@@ -0,0 +1,203 @@
+"""
+Process the raw crosswalk data
+"""
+
+from collections import defaultdict
+import csv
+from typing import Dict, Tuple
+
+
+statenames = {
+    "AK": "Alaska",
+    "AL": "Alabama",
+    "AR": "Arkansas",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DC": "District of Columbia",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NE": "Nebraska",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NV": "Nevada",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VA": "Virginia",
+    "VI": "Virgin Islands",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "West Virginia",
+    "WY": "Wyoming"
+}
+
+def process_zips():
+    # Map fips codes to a list of associated zip codes
+    zip_data = defaultdict(set)
+    with open("raw/zip-county.csv", "r") as f:
+        r = csv.reader(f, delimiter=",")
+        next(r)
+        for row in r:
+            countyfp = row[3]
+            zip = row[0]
+            zip_data[countyfp].add(zip)
+
+    with open("processed/county-zips.csv", "w") as f:
+        w = csv.writer(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        w.writerow(["STCOUNTYFP", "ZIPS,..."])
+        for countyfp,zips in zip_data.items():
+            w.writerow([countyfp] + list(zips))
+
+def get_countyfp_names() -> Dict[int,Tuple[str,str]]:
+    """ Match fips codes to a county name """
+    m = {}
+    # Fill in a few missing values
+    m[46102] = "Oglala Lakota County", "SD"
+    m[46113] = "Shannon County", "SD"
+    with open("raw/zip-county.csv", "r") as f:
+        r = csv.reader(f, delimiter=",")
+        next(r)
+        for row in r:
+            countyfp = row[3]
+            county = row[1]
+            state = row[2]
+            m[countyfp] = county, state
+    return m
+
+def write_countyfp_names():
+    """ Write out table: COUNTYFP,COUNTYNAME,STATE """
+    with open("processed/countyfp-county.csv", "w") as f:
+        f.write("COUNTYFP,COUNTYNAME,STATE\n")
+        for fp,(county,state) in get_countyfp_names().items():
+            f.write(f'"{fp}","{county}","{state}"\n')
+
+def write_state_list():
+    states = set()
+    for fp,(county,state) in get_countyfp_names().items():
+        states.add(state)
+
+    with open("processed/short-states.txt", "w") as f:
+        for s in sorted(states):
+            f.write(f'"{s}": "",\n')
+
+def inverted(d: dict) -> dict:
+    """ Invert key-value pairs """
+    return {v:k for k,v in d.items()}
+
+def process_places():
+    """ Map fips codes to a list of associated places """
+    # County name (not fips) provided in table.
+    # Map (county,state) to a fips code
+    countyfpmap = inverted(get_countyfp_names())
+
+    place_data = defaultdict(set)
+    with open("raw/ansi_places.csv", "r", encoding="latin-1") as f:
+        r = csv.reader(f, delimiter="|")
+        next(r)
+        for row in r:
+            if len(row) == 0:
+                continue
+            place = row[3]
+            state = row[0]
+            counties = [x.strip() for x in row[6].split(",")]
+            # print(counties)
+            for county in counties:
+                cleaned = county.replace("ñ", "n")
+                countyfp = countyfpmap.get((cleaned, state), None)
+                if countyfp is None:
+                    print("No fips code found for ", cleaned, ",", state)
+                else:
+                    place_data[countyfp].add(place)
+
+    with open("processed/county-places.csv", "w") as f:
+        w = csv.writer(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        w.writerow(["STCOUNTYFP", "PLACES,..."])
+        for countyfp,places in place_data.items():
+            w.writerow([countyfp] + list(places))
+
+def process_combined():
+    """
+    Create combined file of counties mapped to states, zips, and places
+    """
+    zips = {}
+    states = {}
+    names = {}
+    places = {}
+
+    with open("processed/county-zips.csv", "r") as f:
+        r = csv.reader(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        next(r)
+        for row in r:
+            countyfp = row[0]
+            ziplist = row[1:]
+            zips[countyfp] = ziplist
+    
+    with open("processed/county-places.csv", "r") as f:
+        r = csv.reader(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        next(r)
+        for row in r:
+            countyfp = row[0]
+            placelist = row[1:]
+            places[countyfp] = placelist
+    
+    with open("processed/countyfp-county.csv", "r") as f:
+        r = csv.reader(f, delimiter=",", quoting=csv.QUOTE_ALL)
+        next(r)
+        for row in r:
+            countyfp = row[0]
+            county = row[1]
+            state = row[2]
+            states[countyfp] = state
+            names[countyfp] = county
+
+    with open("processed/county-all.csv", "w") as f:
+        for fip in zips.keys():
+            name = names.get(fip, "")
+            state = statenames.get(states.get(fip, ""))
+            ziplist = zips.get(fip, [])
+            placelist = places.get(fip, [])
+            s = f"{name}, {state} {' '.join(ziplist)} {' '.join(placelist)}\n"
+            f.write(s)
+
+if __name__ == "__main__":
+    
+    # Find all zip codes associated with each county.
+    process_zips()
+    write_countyfp_names()
+    write_state_list()
+    process_places()
+    process_combined()
